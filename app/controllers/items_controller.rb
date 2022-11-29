a908155fce60b648ee90920cc80120a400317a65
Jeremy Barbara <jeremy.w.barbara@gmail.com>
2022-11-29 13:41:55 -0500

    commit tree 228fbdf54eb5b397a1280bb77dd16e0518233195

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,50 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user
+        items = user.items
+        if items
+          render json: items, include: :user
+        else
+          render json: { error: 'Items not found' }, status: :not_found
+        end
+      else
+        render json: { error: 'User not found' }, status: :not_found
+      end
+
+
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
+  end
+
+  def show
+    item = Item.find_by(id: params[:id])
+    if item
+      render json: item
+    else
+      render json: { error: 'Item not found' }, status: :not_found
+    end
+  end
+
+  def create
+    user = User.find_by(id: params[:user_id])
+
+    if user
+      item = Item.create({
+        name: params[:name],
+        description: params[:description],
+        price: params[:price],
+        user_id: params[:user_id]
+      })
+      render json: item, status: :created
+    else
+      render json: { error: 'User not found' }, status: :not_found
+    end
+
   end
 
 end
